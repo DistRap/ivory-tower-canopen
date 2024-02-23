25cb0cfa4400e26c5f07c252d9bee4581485c04a
sorki <srk@48.io>
2024-02-23 08:26:29 +0100

    IvoryExpr, IvoryRef -> Pointer

--- src/CANOpen/Tower/Utils.hs
+++ src/CANOpen/Tower/Utils.hs
@@ -11,15 +11,16 @@ import Ivory.Tower.HAL.Bus.CAN
 import CANOpen.Tower.Types
 import GHC.TypeLits (KnownNat)
 
-canMsg :: (GetAlloc eff ~ 'Scope s1,
-           IvoryExpr (r s2 ('Stored Uint8)),
-           IvoryExpr (r s2 ('Array m ('Stored Uint8))), IvoryRef r,
-           GHC.TypeLits.KnownNat m)
-       => Uint16
-       -> IBool
-       -> r s2 ('Array m ('Stored Uint8))
-       -> Ix 9
-       -> Ivory eff (ConstRef ('Stack s1) ('Struct "can_message"))
+canMsg
+  :: ( GetAlloc eff ~ 'Scope s1
+     , KnownConstancy c
+     , GHC.TypeLits.KnownNat m
+     )
+  => Uint16
+  -> IBool
+  -> Pointer 'Valid c s2 ('Array m ('Stored Uint8))
+  -> Ix 9
+  -> Ivory eff (ConstRef ('Stack s1) ('Struct "can_message"))
 canMsg uid rtr arr len = do
   let msgid = standardCANID (fromRep uid) (boolToBit rtr)
 
@@ -32,54 +33,58 @@ canMsg uid rtr arr len = do
   --refCopy (reply ~> can_message_buf) arr
   return $ constRef reply
 
-canMsgUint64 :: Uint16
-             -> IBool
-             -> ConstRef s2 ('Stored Uint64)
-             -> Ivory
-                  ('Effects r b ('Scope s1))
-                  (ConstRef ('Stack s1) ('Struct "can_message"))
+canMsgUint64
+  :: Uint16
+  -> IBool
+  -> ConstRef s2 ('Stored Uint64)
+  -> Ivory
+       ('Effects r b ('Scope s1))
+       (ConstRef ('Stack s1) ('Struct "can_message"))
 canMsgUint64 uid rtr x = do
   arr <- local $ izerolen (Proxy :: Proxy 8)
   packInto arr 0 x
   canMsg uid rtr (constRef arr) 8
 
-canMsgUint32 :: Uint16
-             -> IBool
-             -> ConstRef s2 ('Stored Uint32)
-             -> Ivory
-                  ('Effects r b ('Scope s1))
-                  (ConstRef ('Stack s1) ('Struct "can_message"))
+canMsgUint32
+  :: Uint16
+  -> IBool
+  -> ConstRef s2 ('Stored Uint32)
+  -> Ivory
+       ('Effects r b ('Scope s1))
+       (ConstRef ('Stack s1) ('Struct "can_message"))
 canMsgUint32 uid rtr x = do
   arr <- local $ izerolen (Proxy :: Proxy 4)
   packInto arr 0 x
   canMsg uid rtr (constRef arr) 4
 
-canMsgUint16 :: Uint16
-             -> IBool
-             -> ConstRef s2 ('Stored Uint16)
-             -> Ivory
-                  ('Effects r b ('Scope s1))
-                  (ConstRef ('Stack s1) ('Struct "can_message"))
+canMsgUint16
+  :: Uint16
+  -> IBool
+  -> ConstRef s2 ('Stored Uint16)
+  -> Ivory
+       ('Effects r b ('Scope s1))
+       (ConstRef ('Stack s1) ('Struct "can_message"))
 canMsgUint16 uid rtr x = do
   arr <- local $ izerolen (Proxy :: Proxy 2)
   packInto arr 0 x
   canMsg uid rtr (constRef arr) 2
 
-canMsgUint8 :: Uint16
-            -> IBool
-            -> ConstRef s2 ('Stored Uint8)
-            -> Ivory
-                 ('Effects r b ('Scope s1))
-                 (ConstRef ('Stack s1) ('Struct "can_message"))
+canMsgUint8
+  :: Uint16
+  -> IBool
+  -> ConstRef s2 ('Stored Uint8)
+  -> Ivory
+       ('Effects r b ('Scope s1))
+       (ConstRef ('Stack s1) ('Struct "can_message"))
 canMsgUint8 uid rtr x = do
   arr <- local $ izerolen (Proxy :: Proxy 1)
   packInto arr 0 x
   canMsg uid rtr (constRef arr) 1
 
-getStdCANId :: (IvoryExpr (ref s ('Struct "can_message")),
-                IvoryExpr (ref s ('Stored CANArbitrationField)), IvoryRef ref)
-            => ref s ('Struct "can_message")
-            -> Ivory eff Uint32
+getStdCANId
+  :: KnownConstancy c
+  => Pointer 'Valid c s ('Struct "can_message")
+  -> Ivory eff Uint32
 getStdCANId msg = do
   canarbit <- msg ~>* can_message_id
   cid <- assign $ toRep $ canarbit #. can_arbitration_id
